b2ab7a15c3ca9d649a860364be619bc34b8db094
Unknown <dcarr1@gmail.com>
2017-02-01 14:54:02 -0500

    Everything.

---
+++ Poltergun/Assets/Scripts/Reset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Reset : MonoBehaviour {
+
+	public KeyCode resetKey = KeyCode.R;
+
+	void Update() {
+		if (Input.GetKeyDown(resetKey)) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
